54f49bcbb290fc3cd2ee3af7dccfaade1d2fd5c3
MrTheDon <dekaniarz@gmail.com>
2020-11-29 14:24:22 -0500

    added start and options buttons to main

--- Scene/MainMenuScene.cs
+++ Scene/MainMenuScene.cs
@@ -7,14 +7,35 @@ namespace Breakout
     public class MainMenuScene : IScene
     {
         readonly int OFFSET = 50;
-        readonly string MESSAGE = "Press enter to begin!";
+        readonly string MESSAGE = "Press enter to embrace your fate";
+        public bool options = false;
+        public bool start = true;
         
         public void Update(InputState input, GameTime gameTime)
         {
 
-            if (input.WasPressed(Keys.Enter) || input.WasPressed(Buttons.Start))
+            if (input.WasPressed(Keys.Right) || input.WasPressed(Buttons.DPadRight))
             {
+                options = true;
+                start = false;
+            }
+
+            if (input.WasPressed(Keys.Left) || input.WasPressed(Buttons.DPadLeft))
+            {
+                options = false;
+                start = true;
+            }
+
+            if (start == true && (input.WasPressed(Keys.Enter) || input.WasPressed(Buttons.Start)))
+            {
+                options = false;
                 Store.scenes.ChangeScene(SceneName.Game);
+            }
+
+            if (options == true && (input.WasPressed(Keys.Enter) || input.WasPressed(Buttons.Start)))
+            {
+                start = false;
+                Store.scenes.ChangeScene(SceneName.Editor); // todo  come up with options and build the screen
             }
         }
 
@@ -25,6 +46,41 @@ namespace Breakout
                 new Rectangle(0, 0, GameWindow.WIDTH, GameWindow.HEIGHT),
                 Color.White
             );
+
+            if (start)
+            {
+            spriteBatch.Draw(
+                Store.textures.Get(TextureName.PlayButton),
+                new Rectangle(50,GameWindow.HEIGHT - 100,96,54),
+                Color.Yellow
+            );
+            }
+
+            else {
+                spriteBatch.Draw(
+                Store.textures.Get(TextureName.PlayButton),
+                new Rectangle(50,GameWindow.HEIGHT - 100,96,54),
+                Color.White
+                );
+            }
+
+            if (options)
+            {
+             spriteBatch.Draw(
+                Store.textures.Get(TextureName.OptionsButton),
+                new Rectangle(GameWindow.WIDTH - 150,GameWindow.HEIGHT - 100,110,54),
+                Color.Yellow
+                );
+            
+            }
+
+            else {
+                spriteBatch.Draw(
+                Store.textures.Get(TextureName.OptionsButton),
+                new Rectangle(GameWindow.WIDTH - 150,GameWindow.HEIGHT - 100,110,54),
+                Color.White
+                );
+            }
 
             spriteBatch.DrawString(
                 spriteFont,
@@ -37,4 +93,4 @@ namespace Breakout
                 );
         }
     }
-}
+ }
